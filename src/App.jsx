99b2804de8c703d10a2ec3a39138c31c7b6d3d55
Juan Delgado <Jchofy98@gmail.com>
2023-08-30 15:38:09 +0200

    Cambios

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div className='bg-blue-200 min-h-screen flex justify-center items-center flex-col gap-5'>
-    <p className='text-center'>Si te gusta el deporte nuestra comunidad podrás compartir las mejores fotos de futbol</p>
+      <div className="bg-blue-200 min-h-screen flex justify-center items-center flex-col ">
+      <div className="bg-white w-[80%] p-5 rounded-xl border-black border my-5 flex justify-center flex-col items-center">
+        
+        <img src="LogoApuestas.jpg" className="rounded-full w-1/3" alt="" />
+        <p className="text-center text-2xl font-bold mt-2">@GoldWin90</p>
+        <p className="text-center text-xl mb-5">Pronósticos Deportivos IA</p>
+        <p>Quedan <b>3 </b>de 50 plazas</p>
+        
+ 
+        </div>
+        {/* <button className="bg-blue-600 py-2 px-12 rounded-md text-white">
+          Enviar imagen a los administradores
+        </button> */}
 
-    <div className='bg-white w-[70%] p-5 rounded-xl border-black border'>
-        <input type="file" className='w-full'/>
-        <p>Sube una imagen de futbol</p>
-    </div>  
-    <button className='bg-blue-600 py-2 px-12 rounded-md text-white'>Enviar imagen a los administradores</button>
-
-    {/* <a href='https://www.google.com' className='bg-blue-600 py-2 px-12 rounded-md text-white'>Ir google </a> */}
+<p className="mb-5">Únete y gana dinero</p>
+        <a href='https://t.me/Goldwin90' className='bg-blue-600 py-2 px-12 rounded-md text-white'>Telegram</a>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
