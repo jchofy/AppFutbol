45091a06a3f9229bff047b500e64c8b352b18b86
Juan Delgado <Jchofy98@gmail.com>
2023-08-30 16:33:55 +0200

    Add página oculta

--- src/App2.jsx
+++ src/App2.jsx
@@ -10,16 +10,23 @@ function App() {
     <>
       <div className="bg-blue-200 min-h-screen flex justify-center items-center flex-col ">
         <div className="bg-white w-[80%] p-5 rounded-xl border-black border my-5 flex justify-center flex-col items-center">
-        <input type="file" className='w-full'/>
-        <p>Sube una imagen de futbol</p>
+          <img src="LogoApuestas.jpg" className="rounded-full w-1/3" alt="" />
+          <p className="text-center text-2xl font-bold mt-2">@GoldWin90</p>
+          <p className="text-center text-xl mb-5">Pronósticos Deportivos IA</p>
+          <p>
+            Quedan <b>3 </b>de 50 plazas
+          </p>
         </div>
-        
-         
+        {/* <button className="bg-blue-600 py-2 px-12 rounded-md text-white">
+          Enviar imagen a los administradores
+        </button> */}
+
+        <p className="mb-5">Únete y gana dinero</p>
         <a
-          href="https://google.com"
+          href="https://t.me/Goldwin90"
           className="bg-blue-600 py-2 px-12 rounded-md text-white"
         >
-         Enviar imagen a los administradores
+          Telegram
         </a>
       </div>
     </>
